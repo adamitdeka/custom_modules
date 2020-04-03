c12b1ce07f04c1af9098ce2226dfcf5e59cc2a57
Amit Deka <adamitdeka@gmail.com>
2020-04-02 23:11:37 -0400

    add comments in logic.js

--- A4-components-logic.js
+++ A4-components-logic.js
@@ -2,6 +2,9 @@
 //LOGIC FILE
 //IN THIS FILE YOU WILL SIMPLY GRAB YOUR CUSTOM ELEMENT AND ATTACH YOUR METHOD FROM YOUR MODULE TO IT.
 window.onload = ()=>{
+    //the commented line doesn't work. 
+    //humberClockNs.start;
+    //when I tried to call the function using a listener then its working fine
     document.getElementById('template-container').onclick = humberClockNs.start;
 };
 
