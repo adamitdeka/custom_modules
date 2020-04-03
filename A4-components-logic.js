e75094806edeaf0ddf7f3d8902e3fd8d6011cb33
Amit Deka <adamitdeka@gmail.com>
2020-04-02 23:09:00 -0400

    initial commit

---
+++ A4-components-logic.js
@@ -0,0 +1,7 @@
+﻿//ASSIGNMENT 4 WEB COMPONENTS
+//LOGIC FILE
+//IN THIS FILE YOU WILL SIMPLY GRAB YOUR CUSTOM ELEMENT AND ATTACH YOUR METHOD FROM YOUR MODULE TO IT.
+window.onload = ()=>{
+    document.getElementById('template-container').onclick = humberClockNs.start;
+};
+
---
+++ A4-components-module.js
@@ -0,0 +1,47 @@
+﻿//ASSIGNMENT 4 WEB COMPONENTS
+//MODULE FILE
+//IN THIS FILE YOU WILL CREATE YOUR CUSTOM MODULE FOR YOUR REQUIRED FUNCTIONALITY AND EXPOSE IT THROUGH A 'PUBLICLY' ACCESSIBLE METHOD.
+
+var humberClockNs = (function(){  
+    var timeString;
+    var timeStart = function() {
+        var today = new Date();
+        var h = addZero(today.getHours());
+        var m = addZero(today.getMinutes());
+        var s = addZero(today.getSeconds());
+        timeString = h+" : "+m+" : "+s;
+        document.getElementById('time').innerHTML = timeString;
+        setTimeout(timeStart, 500);
+    }
+    function addZero(value){
+        if (value < 10) {value = "0" + value};
+        return value;
+    }
+    var clockModule = {
+        start: timeStart
+    };
+    return clockModule;
+})();
+
+
+
+
+
+
+
+
+
+// var date = new Date();
+//     var hrs = addZero(date.getHours());
+//     var min = addZero(date.getMinutes());
+//     var sec = addZero(date.getSeconds());
+//     function addZero(value) {
+//         if (value < 10) {value = "0" + value}; 
+//         return value;
+//     };  
+//     var clockModule = {
+//         hours: hrs,
+//         minutes: min,
+//         seconds: sec
+//     };
+//     return clockModule;
